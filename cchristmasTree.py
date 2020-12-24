ffc40f8d65de952e4bd622f4a79a2004e436f0a2
patr9k <73367326+patr9k@users.noreply.github.com>
2020-12-24 16:39:49 +0100

    Add files via upload

---
+++ cchristmasTree.py
@@ -0,0 +1,51 @@
+import colorama
+from colorama import Fore, Back, Style
+import os
+import time
+
+def clear():
+    os.system('cls') 
+
+def draw_tree(light):
+    print(f"         {Fore.YELLOW}  / \\")
+    print(f"         {Fore.YELLOW}<     >")
+    print(f"          {Fore.YELLOW}|/ \\|")
+    print(f"{Fore.GREEN}        ******{Fore.RED}q{Fore.GREEN}**")
+    print(f"{Fore.GREEN}        ******{Fore.RED}q{Fore.GREEN}**")
+    print(f"{Fore.GREEN}       ***{Fore.MAGENTA}p{Fore.GREEN}*******")
+    print(f"{Fore.GREEN}       ***{Fore.MAGENTA}p{Fore.GREEN}*******")
+    print(f"{Fore.GREEN}   *****{light}*{Back.RESET}****{Fore.CYAN}o{Fore.GREEN}**{light}*{Back.RESET}******")
+    print(f"{Fore.GREEN}   *****{light}*{Back.RESET}****{Fore.CYAN}o{Fore.GREEN}**{light}*{Back.RESET}******")
+    print(f"{Fore.GREEN}    ****{light}*{Back.RESET}******{Fore.RED}o{Fore.GREEN}******")
+    print(f"{Fore.GREEN}    ****{light}*{Back.RESET}******{Fore.RED}o{Fore.GREEN}******")
+    print(f"{Fore.GREEN}      ******{light}*{Back.RESET}*******")
+    print(f"{Fore.GREEN}      ******{light}*{Back.RESET}*******")
+    print(f"{Fore.GREEN}    ******{light}*{Back.RESET}*{Fore.BLUE}o{Fore.GREEN}*********")
+    print(f"{Fore.GREEN}    ******{light}*{Back.RESET}*{Fore.BLUE}o{Fore.GREEN}*********")
+    print(f"{Fore.GREEN}  *****{Fore.RED}q{Fore.GREEN}*****{light}*{Back.RESET}**{Fore.WHITE}p{Fore.GREEN}*******")
+    print(f"{Fore.GREEN}  *****{Fore.RED}q{Fore.GREEN}*****{light}*{Back.RESET}**{Fore.WHITE}p{Fore.GREEN}*******")
+    print(f"{Fore.GREEN}    *********{Fore.BLUE}o{Fore.GREEN}**{light}*{Back.RESET}*****")
+    print(f"{Fore.GREEN}    *********{Fore.BLUE}o{Fore.GREEN}**{light}*{Back.RESET}*****")
+    print(f"{Fore.GREEN}  *****{Fore.MAGENTA}p{Fore.GREEN}***{light}*{Back.RESET}******{light}*{Back.RESET}*****")
+    print(f"{Fore.GREEN}  *****{Fore.MAGENTA}p{Fore.GREEN}***{light}*{Back.RESET}******{light}*{Back.RESET}*****")
+    print(f"{Fore.GREEN}****{light}*{Back.RESET}**{Fore.WHITE}o{Fore.GREEN}**{light}*{Back.RESET}******{Fore.RED}q{Fore.GREEN}*{light}*{Back.RESET}******")
+    print(f"{Fore.GREEN}****{light}*{Back.RESET}**{Fore.WHITE}o{Fore.GREEN}**{light}*{Back.RESET}******{Fore.RED}q{Fore.GREEN}*{light}*{Back.RESET}******")
+    print(f"           {Back.YELLOW}{Fore.RED}||||")
+    print(f"           {Back.YELLOW}{Fore.RED}||||")
+    print(f"           {Back.YELLOW}{Fore.RED}||||") 
+    
+    print(f"{Fore.RED}      Merry Christmas!") 
+
+colorama.init(autoreset=True)
+
+for x in range(100):
+    clear()
+    if x % 2 == 0:
+        draw_tree(Back.RED)
+    else:
+        draw_tree(Back.CYAN)
+    
+    time.sleep(1)
+
+
+
